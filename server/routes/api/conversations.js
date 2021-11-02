bf8bf1bf3e5c46c3d9f494719d076abee87b45b1
Eljoey <eljoeyy@gmail.com>
2021-11-02 15:37:43 -0500

    issue 1: Bug fixes

--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -19,9 +19,9 @@ router.get("/", async (req, res, next) => {
         },
       },
       attributes: ["id"],
-      order: [[Message, "createdAt", "DESC"]],
+      order: [[Message, "createdAt", "ASC"]],
       include: [
-        { model: Message, order: ["createdAt", "DESC"] },
+        { model: Message, order: ["createdAt", "ASC"] },
         {
           model: User,
           as: "user1",
@@ -68,9 +68,13 @@ router.get("/", async (req, res, next) => {
       }
 
       // set properties for notification count and latest message preview
-      convoJSON.latestMessageText = convoJSON.messages[0].text;
+      convoJSON.latestMessageText = convoJSON.messages[convoJSON.messages.length - 1].text;
+      convoJSON.latestMessageTime = convoJSON.messages[convoJSON.messages.length - 1].createdAt;
       conversations[i] = convoJSON;
     }
+
+    // sort conversations by latest recieved first
+    conversations.sort((a, b) => b.latestMessageTime - a.latestMessageTime);
 
     res.json(conversations);
   } catch (error) {
