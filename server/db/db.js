6851b4b09151b824b59148b671214eadcac013bd
Eljoey <eljoeyy@gmail.com>
2021-11-02 21:32:46 -0500

    remove credentials to .env

--- server/db/db.js
+++ server/db/db.js
@@ -1,11 +1,7 @@
 const Sequelize = require("sequelize");
-require("dotenv").config();
+require('dotenv').config();
 
-const { DB_USERNAME, DB_PASSWORD } = process.env;
-
-const db = new Sequelize('messenger', DB_USERNAME, DB_PASSWORD, {
-  host: 'localhost',
-  dialect: 'postgres',
+const db = new Sequelize(process.env.DB_URL, {
   logging: false
 });
 
