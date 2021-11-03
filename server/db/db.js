ec383b95d7a08ec1fb08f443b7c44c3a7f5df261
Eljoey <eljoeyy@gmail.com>
2021-11-03 12:09:54 -0500

    update db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 require('dotenv').config();
 
-const db = new Sequelize(process.env.DB_URL, {
+const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
   logging: false
 });
 
